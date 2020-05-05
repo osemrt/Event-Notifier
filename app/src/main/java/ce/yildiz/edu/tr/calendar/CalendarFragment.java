b0965e843d8b156055d09c862e853f7adcefd6ea
Mert ÖZ <osemrt@gmail.com>
2020-05-05 18:53:46 +0300

    Add Bottom Navigation Bar

--- app/src/main/java/ce/yildiz/edu/tr/calendar/CalendarFragment.java
+++ app/src/main/java/ce/yildiz/edu/tr/calendar/CalendarFragment.java
@@ -1,15 +1,17 @@
 package ce.yildiz.edu.tr.calendar;
 
 import android.content.Context;
-import android.util.AttributeSet;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -18,7 +20,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class MyCalendarView extends LinearLayout implements View.OnClickListener {
+public class CalendarFragment extends Fragment implements View.OnClickListener {
 
     private final String TAG = this.getClass().getSimpleName();
 
@@ -38,45 +40,36 @@ public class MyCalendarView extends LinearLayout implements View.OnClickListener
     private TextView currentDate;
     private GridView gridView;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        initializeViews(view);
 
-    public MyCalendarView(Context context) {
-        super(context);
-        this.context = context;
+        return view;
     }
 
-    public MyCalendarView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        this.context = context;
-        initializeViews();
+    private void initializeViews(View view) {
 
-    }
-
-    public MyCalendarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    private void initializeViews() {
-        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = layoutInflater.inflate(R.layout.calendar_layout, this);
-
-        nextButton = view.findViewById(R.id.CalendarLayout_Button_Next);
-        prevButton = view.findViewById(R.id.CalendarLayout_Button_Prev);
-        currentDate = view.findViewById(R.id.CalendarLayout_TextView_CurrentDate);
-        gridView = view.findViewById(R.id.CalendarLayout_GridView_Dates);
+        nextButton = view.findViewById(R.id.CalenderFragment_Button_Next);
+        prevButton = view.findViewById(R.id.CalenderFragment_Button_Prev);
+        currentDate = view.findViewById(R.id.CalenderFragment_TextView_CurrentDate);
+        gridView = view.findViewById(R.id.CalenderFragment_GridView_Dates);
 
         nextButton.setOnClickListener(this);
         prevButton.setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.CalendarLayout_Button_Prev:
+            case R.id.CalenderFragment_Button_Prev:
                 calendar.add(Calendar.MONTH, -1);
                 setUpCalendar();
                 break;
-            case R.id.CalendarLayout_Button_Next:
+            case R.id.CalenderFragment_Button_Next:
                 calendar.add(Calendar.MONTH, 1);
                 setUpCalendar();
                 break;
@@ -88,5 +81,4 @@ public class MyCalendarView extends LinearLayout implements View.OnClickListener
     public void setUpCalendar() {
         currentDate.setText(dateFormat.format(calendar.getTime()));
     }
-
 }
